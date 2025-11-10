b540c23fedcfb6a78fd26d4a3b87dbaa92fba864
NachtgeistW <nachtgeistw@outlook.com>
2025-11-10 23:02:13 +0800

    code cleanup

--- scripts/games/star/StarSpawner.cs
+++ scripts/games/star/StarSpawner.cs
@@ -8,29 +8,29 @@ public partial class StarSpawner : Node2D
 {
     [Export] public NodePath CameraPath { get; set; }
 
-    private Camera2D _camera;
-    private IStarGenerator _generator;
-    private PackedScene _starPrefab;
-    private float _nextSpawnX;
-    private List<Node2D> _activeStars = new List<Node2D>();
+    private Camera2D camera;
+    private IStarGenerator generator;
+    private PackedScene starPrefab;
+    private float nextSpawnX;
+    private readonly List<Node2D> activeStars = [];
 
     public override void _Ready()
     {
-        _camera = GetNode<Camera2D>(CameraPath);
-        _generator = new RandomStarGenerator();
-        _starPrefab = GD.Load<PackedScene>("res://prefabs/star.tscn");
+        camera = GetNode<Camera2D>(CameraPath);
+        generator = new RandomStarGenerator();
+        starPrefab = GD.Load<PackedScene>("res://prefabs/star.tscn");
 
-        _nextSpawnX = _camera.GlobalPosition.X + Parameters.StarSpawnDistance;
+        nextSpawnX = camera.GlobalPosition.X + Parameters.StarSpawnDistance;
     }
 
     public override void _Process(double delta)
     {
-        var cameraX = _camera.GlobalPosition.X;
+        var cameraX = camera.GlobalPosition.X;
 
-        while (cameraX + Parameters.StarSpawnDistance >= _nextSpawnX)
+        while (cameraX + Parameters.StarSpawnDistance >= nextSpawnX)
         {
-            SpawnStar(_nextSpawnX);
-            _nextSpawnX += Parameters.StarSpawnInterval;
+            SpawnStar(nextSpawnX);
+            nextSpawnX += Parameters.StarSpawnInterval;
         }
 
         DespawnDistantStars(cameraX);
@@ -38,16 +38,16 @@ public partial class StarSpawner : Node2D
 
     private void SpawnStar(float xPosition)
     {
-        var starPosition = _generator.GenerateStarPosition(xPosition);
-        var star = _starPrefab.Instantiate<Node2D>();
+        var starPosition = generator.GenerateStarPosition(xPosition);
+        var star = starPrefab.Instantiate<Node2D>();
         star.GlobalPosition = starPosition;
         GetParent().AddChild(star);
-        _activeStars.Add(star);
+        activeStars.Add(star);
     }
 
     private void DespawnDistantStars(float cameraX)
     {
-        _activeStars.RemoveAll(star =>
+        activeStars.RemoveAll(star =>
         {
             if (!IsInstanceValid(star))
             {
