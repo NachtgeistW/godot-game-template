056b41d8b42f6c57d68b3b5d1ad133089c6fec2d
夜轮_NachtgeistW <nachtgeistw@protonmail.com>
2025-11-20 11:47:55 +0800

    enable speed up back

--- scripts/games/player/PlayerController.cs
+++ scripts/games/player/PlayerController.cs
@@ -66,7 +66,7 @@ public partial class PlayerController : CharacterBody2D
             UpdateTargetPositionOnPc();
         }
 
-        //IncreaseSpeed();
+        IncreaseSpeed();
         UpdatePlayerPosition();
         
         return;
